293ddbb65f06fcea76fec1b01a5389aa21faa321
Angelohm16 <hmoralesangelof@gmail.com>
2023-06-09 02:46:53 -0500

    fin seccion 5

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,17 +1,29 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Character } from '../../interfaces/character.interface';
 
+
 @Component({
-  selector: 'app-bdz-list',
+  selector: 'dbz-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
 export class ListComponent {
 
   @Input()
-  public CharacterList: Character[] = [{
-    name: 'trunk',
+  public characterList: Character[] = [{
+    name: 'Trunks',
     power: 10
-  }]
+  }];
+
+  // onDelete = Index value : number
+  @Output()
+  public onDelete: EventEmitter<string> = new EventEmitter();
+
+
+  onDeleteCharacter( id?: string ):void {
+
+    if ( !id ) return;
+    this.onDelete.emit( id );
+  }
 
 }
--- src/app/dbz/interfaces/character.interface.ts
+++ src/app/dbz/interfaces/character.interface.ts
@@ -1,7 +1,7 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 
 export interface Character{
+  id?: string;
   name: string;
   power: number;
 }
